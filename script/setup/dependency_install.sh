c5acae191c6fab27105169bb6c91fe86f4af9cab
yhw_yagol <yhw_yagol@yeah.net>
2025-07-22 17:37:16 +0800

    check python3.8.x befor config poetry

--- script/setup/dependency_install.sh
+++ script/setup/dependency_install.sh
@@ -11,12 +11,20 @@ function install_python()
     curl -sSL https://install.python-poetry.org | python3 -
     poetry config virtualenvs.in-project true
 
-    # Install pyenv
-    curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-    pyenv install -v 3.8.10
-    pyenv global 3.8.10
-    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent && poetry env use $(pyenv which python)
-    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_autoware_v2x && poetry env use $(pyenv which python) && poetry install --no-root
+    # Install python3.8.10
+    # Check if installed python3.8.*
+    version=$(python -V 2>&1)
+
+    if [[ "$version" =~ ^Python\ 3\.8\. ]]; then
+        echo "Python 3.8.x is installed: $version"
+    else
+        echo "Python 3.8.x is NOT installed (found: $version)"
+        curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+        pyenv install -v 3.8.10
+        pyenv global 3.8.10
+    fi
+    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent && poetry env use $(which python)
+    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_autoware_v2x && poetry env use $(which python) && poetry install --no-root
 }
 
 function install_rust()
